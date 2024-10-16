4632033211cbf906648a06ca4d72aa933084bd3c
faizanjunani <faizanjunani@yahoo.com>
2024-10-16 16:02:34 +0500

    Updated Maven Compiler version to 17 from 19

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -5,17 +5,18 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features =
-        {/*"src/test/resources/Features/001b-LoginFeature.feature",
-                "src/test/resources/Features/002-ClockIn.feature",
+@CucumberOptions(
+        features =
+        {"src/test/resources/Features/001b-LoginFeature.feature",
+             /*   "src/test/resources/Features/002-ClockIn.feature",
                 "src/test/resources/Features/003-TimeoffFeature.feature",
                 "src/test/resources/Features/004-Timesheet.feature",
                 "src/test/resources/Features/005-ExpenseRequestorFeature.feature",
-                "src/test/resources/Features/006-Clockout.feature",*/
-                "src/test/resources/Features/OnboardingUser.feature"},
+                "src/test/resources/Features/006-Clockout.feature",
+                "src/test/resources/Features/OnboardingUser.feature"*/},
         glue ={"Hooks","StepDefinition"},
         plugin = {"pretty", "html:target/cucumber-reports.html"},
         monochrome = true)
 
-public class Runner {
+public class TestRunner {
 }
