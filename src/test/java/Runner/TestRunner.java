9faa53d326f159aad0af47a898b2f09f5fbcc7da
faizanjunani <faizanjunani@yahoo.com>
2024-10-29 13:14:50 +0500

    Removed keyword driven methods for all the feature files except ExpenseRequestor. Added navigation to homepage on timesheet submission Uncommented expense code from step definition Uncommented feature files from runner

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -8,11 +8,11 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features =
         {"src/test/resources/Features/001b-LoginFeature.feature",
-              /*  "src/test/resources/Features/002-ClockIn.feature",
-                "src/test/resources/Features/003-TimeoffFeature.feature",*/
+                "src/test/resources/Features/002-ClockIn.feature",
+                "src/test/resources/Features/003-TimeoffFeature.feature",
                "src/test/resources/Features/004-Timesheet.feature",
                 "src/test/resources/Features/005-ExpenseRequestorFeature.feature",
-                //"src/test/resources/Features/006-Clockout.feature",
+                "src/test/resources/Features/006-Clockout.feature",
                 //"src/test/resources/Features/OnboardingUser.feature"
                 },
         glue ={"Hooks","StepDefinition"},
